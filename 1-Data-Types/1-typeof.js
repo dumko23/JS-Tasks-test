602ac53c008a4579989cbf6fc50c224396a48c65
Dmytro Parkhomenko <parhomenko.dmit@gmail.com>
2022-05-16 16:53:25 +0300

    Initial commit. Using 'typeof'

---
+++ 1-Data-Types/1-typeof.js
@@ -0,0 +1,9 @@
+ // Initialize constant variable and assign it to value of 5
+ const n = 5;
+// Logging into console the type of a variable n, which is "number"
+ console.log(typeof n); 
+
+// Initialize constant variable and assign it to value "Some string 1234"
+const str = "Some string 1234";
+// Logging into console the type of a variable str, which is "string"
+console.log(typeof str); 
