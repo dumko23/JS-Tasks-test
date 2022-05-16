f7951785124241d6f759db4bd06206fe80f6079e
Dmytro Parkhomenko <parhomenko.dmit@gmail.com>
2022-05-16 17:24:00 +0300

    Operators and string concatenation

---
+++ 1-Data-Types/2-operators.js
@@ -0,0 +1,7 @@
+//Initialize constants a and b, assign a values with 'number' type
+const a = 12;
+const b = 1;
+// Initialize constant sum and assign a summary of a and b
+const sum = a + b; 
+
+console.log(sum);
---
+++ 1-Data-Types/3-concat.js
@@ -0,0 +1,6 @@
+// Initialization and assignation part
+const firstName = "Dima";
+const lastName = "Parkhomenko";
+// Concatenating 2 string variables into 1
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
