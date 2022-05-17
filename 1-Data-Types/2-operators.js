f58ada52d07b29c08e024abbd272b1ad11535209
Dmytro Parkhomenko <parhomenko.dmit@gmail.com>
2022-05-17 11:17:19 +0300

    Deleting unnecessary tasks

--- 1-Data-Types/2-operators.js
+++
@@ -1,7 +0,0 @@
-//Initialize constants a and b, assign a values with 'number' type
-const a = 12;
-const b = 1;
-// Initialize constant sum and assign a summary of a and b
-const sum = a + b; 
-
-console.log(sum);
--- 1-Data-Types/3-concat.js
+++
@@ -1,6 +0,0 @@
-// Initialization and assignation part
-const firstName = "Dima";
-const lastName = "Parkhomenko";
-// Concatenating 2 string variables into 1
-const fullName = firstName + ' ' + lastName;
-console.log(fullName);
