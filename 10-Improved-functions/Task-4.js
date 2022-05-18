5513ba7e16801e0c1850f7a7acc7bd4ecd47b6f0
Dmytro Parkhomenko <parhomenko.dmit@gmail.com>
2022-05-18 17:07:33 +0300

    Task 4 completed

---
+++ 10-Improved-functions/Task-4.js
@@ -0,0 +1,69 @@
+/**
+ * Задача 4.
+ *
+ * Напишите функции compose(), которая в качестве аргументов принимает неограниченное количество функций.
+ *
+ * При запуске compose() последовательно запускает коллбек-функции из аргументов.
+ * 
+ * Важно: compose() выполняет коллбек-функции из аргументов НАЧИНАЯ С КОНЦА.
+ *
+ * Каждая коллбек-функция из цепочки в качестве своего аргумента принимает то, что возвращает предыдущая коллбек-функция.
+ * То есть возвращаемое значение каждой коллбек-функции из цепочки
+ * становится доступным из параметра следующей коллбек-функции в цепочке.
+ *
+ * Функция compose() возвращает ещё одну функцию с одним параметром.
+ * Значение, переданное этой функции в качестве аргумента должно стать
+ * параметром первой коллбек-функции в цепочке выполнения функции compose().
+ *
+ * Итого, подпись функции compose: `compose(f, g)(x) = f(g(x))`.
+ *
+ * Генерировать ошибки если:
+ * - Любой из аргументов не является функцией;
+ * - Любая функция из аргументов не вернула значение.
+ *
+ * Заметка:
+ * Если функции, которая является возвращаемым значением compose()
+ * не передать в качестве аргумента какое-либо значение, генерировать ошибку не нужно.
+ */
+
+// Решение
+
+function compose(...args) {
+    if (args.some(value => typeof value !== 'function')) {
+        throw new Error('All arguments should be Funtions');
+    }
+    let len = args.length;
+    
+    let func = function(arg){
+        let result = arg;
+        for( let i = len-1; i >= 0; i--){
+            if (args[i]() === undefined) {
+                throw new Error('Every function should return some value');
+            } else {
+                result = args[i](result);
+            }
+        }
+        return result;
+    }
+    return func;
+}
+
+
+const result1 = compose(
+    prevResult => prevResult + 'o',
+    prevResult => prevResult + 'l',
+    prevResult => prevResult + 'l',
+    prevResult => prevResult + 'e',
+)('h');
+const result2 = compose(
+    prevResult => prevResult + 'o',
+    prevResult => prevResult + 'l',
+    prevResult => prevResult + 'l',
+    prevResult => prevResult + 'e',
+    () => 'h',
+)();
+
+console.log(result1); // 'hello'
+console.log(result2); // 'hello'
+
+exports.compose = compose;
