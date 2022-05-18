49200dc0ce2d314655de7ca974f7c39bf1fb2fc8
Dmytro Parkhomenko <parhomenko.dmit@gmail.com>
2022-05-18 12:45:06 +0300

    Task 2 completed

---
+++ 6-Arrays/Task-2.js
@@ -0,0 +1,46 @@
+/**
+ * Задача 2.
+ *
+ * Вручную создать имплементацию функции `filter`.
+ * Логика работы ручной имплементации должна быть такой-же,
+ * как и у встроенного метода.
+ *
+ * Генерировать ошибки, если:
+ * - При вызове функции не было передано два аргумента;
+ * - В качестве первого аргумента был передан не массив;
+ * - В качестве второго аргумента была передана не функция.
+ */
+
+const array = ['Доброе утро!', 'Добрый вечер!', 3, 512, '#', 'До свидания!'];
+
+// Решение
+
+function filter(array, callback){
+    let newArray = [];
+    if (!Array.isArray(array)) {
+        throw new Error('First argument should be an Array');
+    } else if (!typeof callback === 'function') {
+        throw new Error('Second argument should be a Function');
+    } else {
+        let len = array.length;
+
+        for (let i = 0; i < len; i++) {
+            if (callback(array[i], i, array)){
+                newArray.push(array[i]);
+            }
+        }
+    }
+    return newArray;
+}
+
+const filteredArray = filter(array, function (item, i, arrayRef) {
+    console.log(item); // элемент массива
+    console.log(i); // индекс элемента
+    console.log(arrayRef); // ссылка на обрабатываемый массив
+
+    return item === 'Добрый вечер!';
+});
+
+console.log(filteredArray); // ['Добрый вечер!']
+
+exports.filter = filter;
