465b1cd4f65520743eba0649296e4a67773f3e62
Dmytro Parkhomenko <parhomenko.dmit@gmail.com>
2022-05-18 12:47:25 +0300

    Task 3 completed

---
+++ 6-Arrays/Task-3.js
@@ -0,0 +1,45 @@
+/**
+ * Задача 3.
+ *
+ * Вручную создать имплементацию функции `every`.
+ * Логика работы ручной имплементации должна быть такой-же,
+ * как и у встроенного метода.
+ *
+ * Генерировать ошибки, если:
+ * - При вызове функции не было передано два аргумента;
+ * - В качестве первого аргумента был передан не массив;
+ * - В качестве второго аргумента была передана не функция.
+ */
+
+const array = [1, 2, 3, 4, 5, 6];
+
+// Решение
+
+function every(array, callback) {
+    if (!Array.isArray(array)) {
+        throw new Error('First argument should be an Array');
+    } else if (!typeof callback === 'function') {
+        throw new Error('Second argument should be a Function');
+    } else {
+        let len = array.length;
+
+        for (let i = 0; i < len; i++) {
+            if (!callback(array[i], i, array)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+const result = every(array, function (item, i, arrayRef) {
+    console.log(item); // элемент массива
+    console.log(i); // индекс элемента
+    console.log(arrayRef); // ссылка на обрабатываемый массив
+
+    return typeof item === 'number';
+});
+
+console.log(result); // true
+
+exports.every = every;
